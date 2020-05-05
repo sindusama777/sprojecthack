94793690e013ff8d8c9a24a86dd9d5e64c10fff2
admspjk <52453747+admspjk@users.noreply.github.com>
2020-05-05 23:40:06 +0700

    Add files via upload

---
+++ RegisterVIPSPJK13.sh
@@ -0,0 +1,54 @@
+hreg(){
+rm -rf /data/tmp/index &> /dev/null
+mkdir /data/tmp/index &> /dev/null
+mkdir /data/tmp/index/A1hbsb &> /dev/null
+mkdir /data/tmp/index/A1hbsvsvscwa &> /dev/null
+mkdir /data/tmp/index/A1hgshshwwub &> /dev/null
+mkdir /data/tmp/index/A1hvsvsgsg &> /dev/null
+mkdir /data/tmp/index/A1hgwuwywuu &> /dev/null
+mkdir /data/tmp/index/A1hhsyeggz &> /dev/null
+mkdir /data/tmp/index/A1hbsvsvv &> /dev/null
+mkdir /data/tmp/index/A1hvsuwueg &> /dev/null
+mkdir /data/tmp/index/A1hbssueuye &> /dev/null
+mkdir /data/tmp/index/A1hbsjaosbvs &> /dev/null
+mkdir /data/tmp/index/A1hbshsueyey &> /dev/null
+mkdir /data/tmp/index/A1hbsbsuwb &> /dev/null
+mkdir /data/tmp/index/A1hbshsuwiwg &> /dev/null
+mkdir /data/tmp/index/A1hbshsheu &> /dev/null
+mkdir /data/tmp/index/A1hbshsisbs &> /dev/null
+mkdir /data/tmp/index/A1hbsjshsvd &> /dev/null
+mkdir /data/tmp/index/A1hbdhsjsiwh &> /dev/null
+mkdir /data/tmp/index/A1hissiisbsbs &> /dev/null
+mkdir /data/tmp/index/A1hbsjsbsbs &> /dev/null
+mkdir /data/tmp/index/A1hbsvsoaoaoauw &> /dev/null
+mkdir /data/tmp/index/A1hbsbscavaja &> /dev/nulla
+mkdir /data/tmp/index/A1hbsjsustwfw &> /dev/null
+mkdir /data/tmp/index/A1hbssjsksvsv &> /dev/null
+mkdir /data/tmp/index/A1hvshsoeudvs &> /dev/null
+mkdir /data/tmp/index/A1hbshshscs  &> /dev/null
+mkdir /data/tmp/index/A1hsass &> /dev/null
+mkdir /data/tmp/index/A1hbbvv &> /dev/null
+mkdir /data/tmp/index/A1hisjjs &> /dev/null
+mkdir /data/tmp/index/A1hwowowowo &> /dev/null
+mkdir /data/tmp/index/A1hvsvshs &> /dev/null
+#
+touch /data/tmp/index/A1hbsjshsvd/.reg &> /dev/null
+sleep 1
+rm -rf RegisterVIPSPJK13.sh
+echo ""
+echo ""
+echo "Done Registering....."
+exit 0;
+}
+
+echo "Register Your Device To VIP SPJK S13......"
+sleep 1
+echo "Wait..."
+if [ ! -d "/data/tmp" ]; &> /dev/null
+then
+mkdir /data/tmp &> /dev/null
+fi
+reg
+echo ""
+echo "Register Failed"
+echo "Tell @admsprojecthack for register"
